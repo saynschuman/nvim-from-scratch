88b6dafacc02659ca045bd8b150b93d30a1385c0
vitaliy <vitaliy@heliotrope.digital>
2023-03-18 19:17:00 +0200

    comments added

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
+-- Отключение создания файлов swap
 vim.o.swapfile = false
+
+-- Включение отображения номеров строк
 vim.opt.number = true
+
 -- Определяем функцию для отображения файлового менеджера
 function open_filemanager()
   -- Если менеджер уже открыт, скрываем его
@@ -11,17 +15,25 @@ function open_filemanager()
     vim.g.filemanager_open = true
   end
 end
+
+-- Отключение отображения относительных номеров строк
 vim.opt.relativenumber = false 
+
 -- Установка leader
 vim.g.mapleader = ' '
 
+-- Определение команды для перезагрузки файла конфигурации
 vim.cmd('command! ReloadConfig source ~/.config/nvim/init.lua')
--- Перезагрузка файла конфигурации
+
+-- Установка комбинации клавиш для перезагрузки файла конфигурации
 vim.api.nvim_set_keymap('n', '<leader>r', ':ReloadConfig<CR>', { noremap = true, silent = true })
+
 -- Отключение отображения символа тильда в незанятых строках
 vim.opt.fillchars:remove('eob:~')
--- remove tilda at the end of buffer
+
+-- Установка пустого символа в конце буфера
 vim.wo.fillchars='eob: '
+
 -- Включение использования системного буфера обмена
 vim.opt.clipboard:append('unnamedplus')
 
@@ -31,12 +43,16 @@ vim.cmd [[
   highlight StatusLineNC ctermfg=8 ctermbg=7 guifg=#ffffff guibg=#ffffff
 ]]
 
+-- Отключение баннера в netrw
 vim.g.netrw_banner = 0
-vim.g.netrw_list_style = 3
--- vim.g.netrw_browse_split = 3
 
+-- Изменение стиля списка файлов в netrw
+vim.g.netrw_list_style = 3
+
+-- Задание размера окна для netrw
 vim.g.netrw_winsize = 25
 
+-- Определение функции для открытия/закрытия файлового менеджера
 function toggle_filemanager()
   -- Если менеджер уже открыт, закрываем его и удаляем буфер
   if vim.g.filemanager_open then
@@ -49,18 +65,19 @@ function toggle_filemanager()
   end
 end
 
--- Определяем функцию для установки комбинации клавиш
+-- Определение функции для установки комбинации клавиш
 function setup_keymap()
   vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua toggle_filemanager()<CR>', { noremap = true, silent = true })
 end
 
+-- Установка комбинации клавиш для открытия/закрытия файлового менеджера
 setup_keymap()
 
--- Переназначаем Shift+h на перемещение к предыдущей вкладке
+-- Переназначение комбинации клавиш Shift+h на перемещение к предыдущей вкладке
 vim.api.nvim_set_keymap('n', '<S-h>', ':tabprevious<CR>', { noremap = true, silent = true })
 
--- Переназначаем Shift+l на перемещение к следующей вкладке
+-- Переназначение комбинации клавиш Shift+l на перемещение к следующей вкладке
 vim.api.nvim_set_keymap('n', '<S-l>', ':tabnext<CR>', { noremap = true, silent = true })
 
+-- Установка комбинации клавиш для закрытия текущего буфера
 vim.api.nvim_set_keymap('n', '<leader>c', ':bd<CR>', { noremap = true, silent = true })
-
