a9490b5e4266b22a834940733dff77cc64aad853
vitaliy <vitaliy@heliotrope.digital>
2023-03-18 19:14:06 +0200

    removed file_manager

--- init.lua
+++ init.lua
@@ -1,5 +1,17 @@
+vim.o.swapfile = false
 vim.opt.number = true
-vim.opt.relativenumber = true
+-- Определяем функцию для отображения файлового менеджера
+function open_filemanager()
+  -- Если менеджер уже открыт, скрываем его
+  if vim.g.filemanager_open then
+    vim.cmd(':Lexplore')
+  else
+    -- Иначе открываем менеджер и устанавливаем флаг
+    vim.cmd(':Lexplore --immediate')
+    vim.g.filemanager_open = true
+  end
+end
+vim.opt.relativenumber = false 
 -- Установка leader
 vim.g.mapleader = ' '
 
@@ -13,21 +25,42 @@ vim.wo.fillchars='eob: '
 -- Включение использования системного буфера обмена
 vim.opt.clipboard:append('unnamedplus')
 
-
 -- Определение нового цвета для StatusLine
 vim.cmd [[
-  highlight StatusLine ctermfg=0 ctermbg=0 guifg=#000000 guibg=#000000
-  highlight StatusLineNC ctermfg=0 ctermbg=0 guifg=#000000 guibg=#000000
+  highlight StatusLine ctermfg=8 ctermbg=7 guifg=#ffffff guibg=#ffffff
+  highlight StatusLineNC ctermfg=8 ctermbg=7 guifg=#ffffff guibg=#ffffff
 ]]
 
--- Bзменение цвета номеров строк
--- vim.cmd [[
---  highlight LineNr ctermfg=0 ctermbg=0 guifg=#880000 guibg=#880000
---  highlight CursorLineNr ctermfg=1 ctermbg=1 guifg=#000000 guibg=#000000
--- ]]
+vim.g.netrw_banner = 0
+vim.g.netrw_list_style = 3
+-- vim.g.netrw_browse_split = 3
 
+vim.g.netrw_winsize = 25
 
--- init.lua
--- Импорт модуля file_manager и вызов функции file_manager
-vim.api.nvim_set_keymap('n', '<Leader>f', ':lua require("file_manager")()<CR>', {noremap = true, silent = true})
+function toggle_filemanager()
+  -- Если менеджер уже открыт, закрываем его и удаляем буфер
+  if vim.g.filemanager_open then
+    vim.cmd(':bd!')
+    vim.g.filemanager_open = false
+  else
+    -- Иначе открываем менеджер и устанавливаем флаг
+    vim.cmd(':Lexplore')
+    vim.g.filemanager_open = true
+  end
+end
+
+-- Определяем функцию для установки комбинации клавиш
+function setup_keymap()
+  vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua toggle_filemanager()<CR>', { noremap = true, silent = true })
+end
+
+setup_keymap()
+
+-- Переназначаем Shift+h на перемещение к предыдущей вкладке
+vim.api.nvim_set_keymap('n', '<S-h>', ':tabprevious<CR>', { noremap = true, silent = true })
+
+-- Переназначаем Shift+l на перемещение к следующей вкладке
+vim.api.nvim_set_keymap('n', '<S-l>', ':tabnext<CR>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>c', ':bd<CR>', { noremap = true, silent = true })
 
