5a0a57660ab5569233ca53f23f179deb9a18927d
vitaliy <vitaliy@heliotrope.digital>
2023-04-04 23:39:38 +0300

    cursor fixed

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 require("vitaliy.options")
 require("vitaliy.lazy")
-require("vitaliy.theme")
 require("vitaliy.mappings")
+require("vitaliy.theme")
+
+vim.cmd("autocmd VimEnter * Neotree")
+vim.cmd('set guicursor+=n:hor20-Cursor/lCursor')
+vim.cmd('set guicursor+=v:hor10-Cursor/lCursor')
 
 
