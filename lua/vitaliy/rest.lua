4928c096d6646394a002ba5e859618d7ad99a05d
vitaliy <vitaliy@heliotrope.digital>
2023-04-07 19:47:34 +0300

    ability to get request

--- lua/vitaliy/rest.lua
+++ lua/vitaliy/rest.lua
@@ -1,7 +1,20 @@
 local M = {}
 
-M.exec = function()
-  print("It works")
+local function get_request(url)
+  local cmd = "curl -s -o - " .. url
+  local handle = io.popen(cmd)
+
+  if handle == nil then return nil end
+
+  local response = handle:read("*a")
+  handle:close()
+
+  -- Открываем новый вертикальный split-окно и выводим ответ в нем
+  vim.api.nvim_command("vnew")
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(response, "\n", {}))
 end
+
+M.exec =
+    function() get_request("https://jsonplaceholder.typicode.com/todos/1") end
 
 return M
